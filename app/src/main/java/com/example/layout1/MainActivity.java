0e7196d7d951395f81d612f9fcb415b98938ea91
SisiliaIndah18 <79432738+SisiliaIndah18@users.noreply.github.com>
2021-03-08 12:54:18 +0700

    Tugas Pengembangan Aplikasi Mobile

--- app/src/main/java/com/example/layout1/MainActivity.java
+++ app/src/main/java/com/example/layout1/MainActivity.java
@@ -2,13 +2,56 @@ package com.example.layout1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnLogin;
+    EditText edemail, edpassword;
+    String nama, password;
 
+    public boolean ConfirmData(){
+        String EmailKey = "ndh1994@gmail.com";
+        String PwKey = "2001";
+        if (EmailKey.equals(edemail.getText().toString()) && PwKey.equals(edpassword.getText().toString()))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.LoginBtn);
+        edemail = findViewById(R.id.IdEmail);
+        edpassword = findViewById(R.id.IdPw);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+                if(ConfirmData() == true){
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email Anda : " +nama+ " dan Password Anda : "+password+ "", Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email / Password Salah !!", Toast.LENGTH_LONG);
+                    t.show();
+                }
+
+            }
+        });
     }
 }
