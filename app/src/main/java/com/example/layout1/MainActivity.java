68ced4c9fd5ccca39cc37c9de2e2e25bf1a74f31
SisiliaIndah18 <79432738+SisiliaIndah18@users.noreply.github.com>
2021-03-08 15:42:57 +0700

    Sisilia Indah Lestari-20190140006

--- app/src/main/java/com/example/layout1/MainActivity.java
+++ app/src/main/java/com/example/layout1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.layout1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,21 +11,11 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     Button btnLogin;
     EditText edemail, edpassword;
-    String nama, password;
+    String EmailKey, PwKey;
 
-    public boolean ConfirmData(){
-        String EmailKey = "ndh1994@gmail.com";
-        String PwKey = "2001";
-        if (EmailKey.equals(edemail.getText().toString()) && PwKey.equals(edpassword.getText().toString()))
-        {
-            return true;
-        }
-        return false;
-    }
-
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,21 +28,21 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nama = edemail.getText().toString();
-                password = edpassword.getText().toString();
 
-                if(ConfirmData() == true){
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Email Anda : " +nama+ " dan Password Anda : "+password+ "", Toast.LENGTH_LONG);
-                    t.show();
+                EmailKey = "ndh1994@gmail.com";
+                PwKey = "2001";
+                if (EmailKey.equals(edemail.getText().toString()) && PwKey.equals(edpassword.getText().toString())){
+                    //jika login berhasil
+                    Toast s = Toast.makeText(getApplicationContext(), "Login Sukses Email Anda : " +EmailKey+ " dan Password Anda : "+PwKey+ "", Toast.LENGTH_LONG);
+                        s.show();
+                }else {
+                    //jika login gagal
+                    Toast s = Toast.makeText(getApplicationContext(), "Email / Password Salah !!", Toast.LENGTH_SHORT);
+                        s.show();
                 }
-                else {
-                    Toast t = Toast.makeText(getApplicationContext(),
-                            "Email / Password Salah !!", Toast.LENGTH_LONG);
-                    t.show();
-                }
-
             }
+
         });
+
     }
 }
